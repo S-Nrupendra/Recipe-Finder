95d924fab0833143ba5e39a49c07fe8a420092aa
S-Nrupendra <snrupendra@gmail.com>
2026-02-18 20:14:39 +0530

    api update

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ export const fetchRecipes = async(query = "") =>{ // Default to empty string if 
     // Loads recipes from the API based on the search query
     try{
         const res = await fetch(
-            `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`, {cache:"no-store"}
         );
         const data = await res.json();
         return data.meals || [];
