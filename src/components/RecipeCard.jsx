2649de483ad0c90fbcc65b821c6461520fca504b
S-Nrupendra <snrupendra@gmail.com>
2026-02-18 19:44:13 +0530

    Card Design

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./styles/RecipeCard.css"
 
-const RecipeCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const RecipeCard = ({recipe}) => {
+  return <div className='recipe-card'>
+    <img src={recipe.strMealThumb} alt=''/>
+    <h3>{recipe.strMeal}</h3>
+    <p>{recipe.strCategory}</p>
+  </div>
 }
 
 export default RecipeCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { fetchRecipes } from '../utils/api';
+import RecipeCard from '../components/RecipeCard';
+import "./Home.css"
 
 const Home = () => {
   const [chickenRecipes, setChickenRecipes] = useState([]);
@@ -18,12 +20,19 @@ const Home = () => {
     fetchData();
   },[])
 
-  return (
-    <div>
-      Home
+  return( <div className='home-container'>
+      <div className='section'>
+        <h2>Chicken Recipes</h2>
+        <div className='recipe-grid'>
+          {
+            chickenRecipes.map((r) => (
+              <RecipeCard key={r.idMeal} recipe={r}/>
+            ))
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
-Home
